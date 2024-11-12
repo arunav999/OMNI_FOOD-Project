f1ee2d9a6cc42eb8e64a238c349519a006e7181d
Arunav Singh <arunavsingh999@gmail.com>
2024-11-12 20:52:26 +0530

    completed project

--- JS/script.js
+++ JS/script.js
@@ -57,6 +57,29 @@ navTryEl.addEventListener("click", handleCloseClick);
 //   headerEl.classList.toggle("nav-open");
 // }
 
+// Sticky
+
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    if (!ent.isIntersecting) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
+
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
